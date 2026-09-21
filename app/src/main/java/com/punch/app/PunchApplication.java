fe81f73062396018f7eef39c035fa25c50f4572c
LiaoZhongHao <liao08022040@gmail.com>
2026-09-21 19:41:50 +0800

    feat: auto-prepare Wi-Fi scan access at app start

--- app/src/main/java/com/punch/app/PunchApplication.java
+++ app/src/main/java/com/punch/app/PunchApplication.java
@@ -20,6 +20,7 @@ import com.punch.app.service.HeartbeatManager;
 import com.punch.app.service.SyncCoordinator;
 import com.punch.app.utils.AppLogger;
 import com.punch.app.utils.KioskManager;
+import com.punch.app.utils.PlatformPermissionBootstrap;
 import com.punch.app.utils.SessionManager;
 import com.punch.app.utils.UpdateManager;
 import com.punch.app.receiver.UpdateRetryReceiver;
@@ -97,6 +98,8 @@ public class PunchApplication extends Application {
             SessionManager.get().init(this);
             return;
         }
+
+        PlatformPermissionBootstrap.prepareWifiScanAccess(this);
 
         registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
             @Override
