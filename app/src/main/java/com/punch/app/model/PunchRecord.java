2500e99c357c89634f8504a04568faf81bdf6cf1
LiaoZhongHao <liao08022040@gmail.com>
2026-09-23 00:26:48 +0800

    model: add durable punch lifecycle state

--- app/src/main/java/com/punch/app/model/PunchRecord.java
+++ app/src/main/java/com/punch/app/model/PunchRecord.java
@@ -1,45 +1,30 @@
 package com.punch.app.model;
 
-
 public class PunchRecord {
-    
+    public static final String STATE_ACCEPTING = "ACCEPTING";
+    public static final String STATE_ACCEPTED = "ACCEPTED";
+    public static final String STATE_UPLOADING = "UPLOADING";
+    public static final String STATE_SYNCED = "SYNCED";
+
     public String id;
-    
     public String clientRecordId;
-    
     public String empId;
-    
     public String empName;
-    
     public String dept;
-    
     public long punchTime;
-    
     public String punchDate;
-    
     public String punchType;
-    
     public String shiftName;
-    
     public String lineCode;
-
     public int teamBindingId;
-
     public int clockIndex;
-
     public double matchScore;
-
     public String snapImagePath;
-
     public String snapImageMimeType;
-
     public int snapImageWidth;
-
     public int snapImageHeight;
-
     public long snapImageSize;
-
     public long snapCapturedAt;
-    
     public int isSynced;
+    public String punchState = STATE_ACCEPTED;
 }
